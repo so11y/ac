944a33bd16eae03fb7ed4c235459ddce165ec118
sorry <247076126@qq.com>
2023-06-01 13:24:54 +0800

    commit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { ColorTween, Tween } from "./Tween";
 
 const div1 = document.querySelector("#div1")! as HTMLElement;
 const div2 = document.querySelector("#div2")! as HTMLElement;
+const div3 = document.querySelector("#div3")! as HTMLElement;
 const ac = new AnimationController(1000);
 const t1 = new Tween(30, 100).animate(ac).builder((value) => {
   div1.style.width = `${value}px`;
@@ -15,8 +16,9 @@ const t2 = new Tween(
   [100, new ColorTween("#200531"), 0],
   [200, new ColorTween("#124618"), 60]
 )
-// .animate(ac)  然后把下面的 t1.then(t2); 删掉 可以一个控制器控制多个动画
-  .animate(new AnimationController(3000))
+  // .animate(ac)  然后把下面的 t1.then(t2); 删掉 可以一个控制器控制多个动画
+  // .animate(new AnimationController(3000))
+  .animate(ac)
   .builder((value) => {
     const [height, backgroundColor, marginLeft] = value;
     div2.style.width = `100px`;
@@ -25,7 +27,10 @@ const t2 = new Tween(
     div2.style.backgroundColor = backgroundColor;
   });
 
-t1.then(t2);
+new Tween(0, 1239599).animate(ac).builder((value) => {
+  div3.textContent = Number(value.toFixed(0)).toLocaleString();
+});
+// t1.then(t2);
 
 document.querySelector("#button1")!.addEventListener("click", () => ac.play());
 document
